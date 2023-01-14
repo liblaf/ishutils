eb09e778b163601e6327bb9d7dace2154ae37714
Qin Li <30631553+liblaf@users.noreply.github.com>
2023-01-14 21:18:45 +0800

    fix(action.run): add more options

--- ishutils/action/run.py
+++ ishutils/action/run.py
@@ -1,11 +1,29 @@
 import shlex
 import subprocess
+from pathlib import Path
 
 from ..log import Action, get_logger
 
 
-def run(*args: str) -> subprocess.CompletedProcess:
+def run(
+    *args: str | Path,
+    stdin=None,
+    stdout=None,
+    stderr=None,
+    cwd=None,
+    capture_output: bool = False,
+    check_returncode: bool = True,
+) -> subprocess.CompletedProcess:
     logger = get_logger()
-    logger.running(action=Action.RUN, message="+ " + shlex.join(args))
-    complete: subprocess.CompletedProcess = subprocess.run(args=args)
-    return complete
+    logger.running(action=Action.RUN, message="+ " + shlex.join(list(map(str, args))))
+    process: subprocess.CompletedProcess = subprocess.run(
+        args=args,
+        stdin=stdin,
+        stdout=stdout,
+        stderr=stderr,
+        cwd=cwd,
+        capture_output=capture_output,
+    )
+    if check_returncode:
+        process.check_returncode()
+    return process
